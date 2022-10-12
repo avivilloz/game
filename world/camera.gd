6b509df0cb0e4ae23e43785b0b7e2b1808c03a75
avivilloz <avivilloz@gmail.com>
2022-10-12 19:49:13 +0300

    ur

---
+++ world/camera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_node("/root/world/player")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x = player.position.x
+	position.y = player.position.y
